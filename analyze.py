c4bc319860d4c0f032bc0877a6d5fb4d2a734080
Ryan Sheehan <rsheehan@gmail.com>
2015-03-02 16:56:07 -0600

    made pep8 compliant

--- analyze.py
+++ analyze.py
@@ -5,43 +5,50 @@ import sqlite3
 
 
 # Get the information for the requested team
-    # conn - database connection
-    # teamString - prefix for the team requested (Home or Away)
-def writeTeamInfo( conn, outputFile, teamString ):
+# conn - database connection
+# teamString - prefix for the team requested (Home or Away)
+def writeTeamInfo(conn, outputFile, teamString):
+    ''' Outputs team information to the file
+    '''
 
-    #give a title for the team
+    # give a title for the team
     outputFile.write("{0} Team\n".format(teamString))
 
     # run the query to get the team data from the db
     sql = ('SELECT pl.ID, '
-        'pl.strName AS Name, '
-        't.DATA_CONSTANT AS Type, '
-        'ROUND(pl.Characteristics_fMovementAllowance * 0.12, 0) AS MV, '
-        'ROUND(pl.Characteristics_fStrength * 0.06, 0) AS ST, '
-        'ROUND(pl.Characteristics_fAgility * 0.06, 0) AS AG, '
-        'ROUND(pl.Characteristics_fArmourValue * 0.11, 0) AS AV, '
-        'pl.idPlayer_Levels AS Level, '
-        'pl.iExperience AS Exp, '
-        'pl.iValue AS Value, '
-        'Group_Concat(DISTINCT ts.DESCRIPTION) AS DefaultSkills, '
-        'Group_Concat(DISTINCT mainsl.DATA_CONSTANT) AS LevelSkills '
-        'FROM {0}_Player_Listing pl '
-        'LEFT OUTER JOIN {0}_player_types t ON pl.idPlayer_Types = t.ID '
-        'LEFT OUTER JOIN {0}_player_Type_Skills ts ON pl.idPlayer_Types = ts.idPlayer_Types '
-        'LEFT OUTER JOIN {0}_Player_Skills ps ON pl.ID = ps.idPlayer_Listing '
-        'LEFT OUTER JOIN MainDB.Skill_Listing mainsl ON ps.idSkill_Listing = mainsl.ID '
-        'GROUP BY pl.ID'
-        ).format(teamString)
+           'pl.strName AS Name, '
+           't.DATA_CONSTANT AS Type, '
+           'ROUND(pl.Characteristics_fMovementAllowance * 0.12, 0) AS MV, '
+           'ROUND(pl.Characteristics_fStrength * 0.06, 0) AS ST, '
+           'ROUND(pl.Characteristics_fAgility * 0.06, 0) AS AG, '
+           'ROUND(pl.Characteristics_fArmourValue * 0.11, 0) AS AV, '
+           'pl.idPlayer_Levels AS Level, '
+           'pl.iExperience AS Exp, '
+           'pl.iValue AS Value, '
+           'Group_Concat(DISTINCT ts.DESCRIPTION) AS DefaultSkills, '
+           'Group_Concat(DISTINCT mainsl.DATA_CONSTANT) AS LevelSkills '
+           'FROM {0}_Player_Listing pl '
+           'LEFT OUTER JOIN {0}_player_types t ON pl.idPlayer_Types = t.ID '
+           'LEFT OUTER JOIN {0}_player_Type_Skills ts '
+           'ON pl.idPlayer_Types = ts.idPlayer_Types '
+           'LEFT OUTER JOIN {0}_Player_Skills ps '
+           'ON pl.ID = ps.idPlayer_Listing '
+           'LEFT OUTER JOIN MainDB.Skill_Listing mainsl '
+           'ON ps.idSkill_Listing = mainsl.ID '
+           'GROUP BY pl.ID'
+           ).format(teamString)
 
     rows = [', '.join([str(col) for col in row]) for row in conn.execute(sql)]
     outputFile.write('\n'.join(rows) + '\n')
+
 
 if __name__ == '__main__':
 
     # Starting point
     if len(sys.argv) > 1:
         if not os.path.isfile(sys.argv[1]):
-            raise ValueError("'{0}' is an invalid file name.".format(sys.argv[1]))
+            raise ValueError(
+                "'{0}' is an invalid file name.".format(sys.argv[1]))
         file = os.path.basename(sys.argv[1])
 
     else:
@@ -49,10 +56,10 @@ if __name__ == '__main__':
     print("Input: {0}".format(file))
 
     conn = sqlite3.connect(file)
-    print("Opened database successfully");
+    print("Opened database successfully")
 
     conn.execute("ATTACH DATABASE 'WorldCup.db' AS MainDB")
-    print("Attached MainDB");
+    print("Attached MainDB")
 
     outputFileName = os.path.splitext(file)[0] + "_Summary.txt"
     print("Output file: {0}".format(outputFileName))
